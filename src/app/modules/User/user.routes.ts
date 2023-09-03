49334b6a6023905725c5a507d96c29d73d4aa2ad
maheenur13 <differentmahee13@gmail.com>
2023-09-03 08:52:59 +0600

    user update validation done

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -2,11 +2,18 @@ import express from 'express';
 import { userController } from './user.controller';
 import auth from '../../middlewares/auth';
 import { ENUM_USER_ROLE } from '../../../enums/user';
+import validateRequest from '../../middlewares/validateRequest';
+import { userValidation } from './user.validation';
 const router = express.Router();
 
 router.get('/', auth(ENUM_USER_ROLE.ADMIN), userController.getAllUser);
 router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), userController.getSingleUser);
 router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), userController.deleteUser);
-router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), userController.updateUser);
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
+  validateRequest(userValidation.updateUser),
+  userController.updateUser
+);
 
 export const userRoutes = router;
--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -1,2 +1,17 @@
+import { z } from 'zod';
 
-// Define your validations here
+const updateUser = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    email: z.string().optional(),
+    password: z.string().optional(),
+    role: z.enum(['customer', 'admin']).optional(),
+    contactNo: z.string().optional(),
+    address: z.string().optional(),
+    profileImg: z.string().optional(),
+  }),
+});
+
+export const userValidation = {
+  updateUser,
+};
